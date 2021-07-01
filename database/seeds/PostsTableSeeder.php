640f65488861b7409014614989861290a0817c7e
Merlino79 <alessandro.ciancio@hotmail.com>
2021-07-01 18:01:07 +0200

    Post Table seeder

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Post;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class PostsTableSeeder extends Seeder
 {
@@ -11,6 +13,13 @@ class PostsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+    for ($i = 0; $i < 10; $i++){
+      $new_post = new Post();
+      $new_post->title = "Titolo post " . ($i + 1);
+      $new_post->slug = Str::slug($new_post->title, '-');
+      $new_post->content = ($i + 1) . " La sigaretta elettronica è un inalatore che vaporizza un liquido, simulando così il fumo del tabacco delle sigarette. Ultimamente se ne vedono parecchie in giro, e ancora non ci abbiamo fatto l’abitudine: vediamo gente che sbuffa fumo in luoghi chiusi in grande tranquillità, mentre noi sgraniamo gli occhi, e poi ci accorgiamo che invece di una sigaretta hanno in mano una specie di tubetto. Il fumo della sigaretta elettronica non puzza.";
+      $new_post->save();
     }
+}
+
 }
