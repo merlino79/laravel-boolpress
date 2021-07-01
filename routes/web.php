45e2f9aadf3f3fe5b034b6bce01a9789bc225455
Merlino79 <alessandro.ciancio@hotmail.com>
2021-07-01 17:03:29 +0200

    cambio rotta home in admin nel server provider

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,11 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+
+Route::prefix('admin')
+    ->namespace('Admin')
+    ->middleware('auth')
+    ->name('admin.')
+    ->group(function(){
+        route::get('/','HomeController@index')->name('home');
+    });
