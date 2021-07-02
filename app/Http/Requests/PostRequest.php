0903db704c9f94555c32af4e84d1ec945c229176
Merlino79 <alessandro.ciancio@hotmail.com>
2021-07-02 16:26:08 +0200

    controllo errori nell'edit

--- app/Http/Requests/PostRequest.php
+++ app/Http/Requests/PostRequest.php
@@ -24,7 +24,7 @@ class PostRequest extends FormRequest
     public function rules()
     {
         return [
-            'title'=>'required|max:15',
+            'title'=>'required|max:255',
             'content'=>'required|min:3'
         ];
     }
--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -3,13 +3,21 @@
 @section('content')
 <div class="container">
    <div class="row">
-     <div class="col">
-       <h1>{{ $post->title }}</h1>
-        <a class="btn btn-primary" href="{{ route('admin.posts.show', $post) }}">SHOW</a>
-     </div>
+      <div class="col">
+        <h1>{{ $post->title }}</h1>
+          <a class="btn btn-primary" href="{{ route('admin.posts.show', $post) }}">SHOW</a>
+      </div>
      
    </div>    
-       
+       @if ($errors->any())
+      <div class="alert alert-danger">
+        <ul>
+          @foreach ($errors->all() as $error )
+            <li>{{ $error }}</li>
+          @endforeach
+        </ul>
+      </div>
+    @endif  
 </div>
 <div class="container">
   <div class="row">
@@ -22,12 +30,22 @@
 
             <div class="mb-3">
                 <label class="label-control" for="title">Titolo</label>
-                <input type="text" id="title" name="title" value="{{ $post->title }}" class="form-control">
+                <input type="text" id="title" name="title" value="{{ old('title',$post->title) }}" class="form-control  @error('title') is-invalid @enderror" >
+                @error('title')
+                  <div class="text-danger">
+                    {{ $message }}
+                  </div>
+                @enderror
             </div>
 
             <div class="mb-3">
                 <label class="label-control" for="content">Content</label>
-                <textarea type="text" id="content" name="content" class="form-control" rows="5" >{{ $post->content }}</textarea>
+                <textarea type="text" id="content" name="content" class="form-control @error('content') is-invalid @enderror" rows="5" >{{ old('content',$post->content) }}</textarea>
+                @error('content')
+                  <div class="text-danger">
+                    {{ $message}}
+                  </div>
+                @enderror
             </div>
             <div>
                 <button class="btn btn-primary" type="submit">Invio</button>
