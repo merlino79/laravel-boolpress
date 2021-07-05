898fa916f6fe2bc43152b6dfd655f6647653a919
Merlino79 <alessandro.ciancio@hotmail.com>
2021-07-05 17:43:48 +0200

    non creato id category

---
+++ database/migrations/2021_07_05_152926_update_posts_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            // creo la colonna che diventerà FK
+            $table->unsignedBigInteger('category_id')->nullable()->after('id');
+
+            //setto la colonna creata come FK
+            $table->foreign('category_id')
+                    ->references('id')
+                    ->on('categories')
+                    ->onDelete('set null');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            // elimino la FK
+            $table->dropForeign(['category_id']);
+
+            //elimino la colonna
+            $table->dropColumn('category_id');
+        });
+    }
+}
