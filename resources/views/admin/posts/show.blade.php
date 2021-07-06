a23b53c49515c177faf6735b2596139ba037e470
Merlino79 <alessandro.ciancio@hotmail.com>
2021-07-06 17:51:35 +0200

    edit

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -15,6 +15,16 @@
         <p>{{ $post->content }}</p>
        
      </div>
+     <div class="container">
+       <div class="row">
+         <div class="col mb-4">
+           @foreach($post->tags as $tag)
+              <span class="badge badge-primary">{{ $tag->name }}</span>
+           @endforeach
+         </div>
+         <p>{{ $post->name }}</p>
+       </div>
+     </div>
      <div class="col">
         <div>
         <a class="btn btn-info" href="{{ route('admin.posts.edit', $post) }}">EDIT</a>
